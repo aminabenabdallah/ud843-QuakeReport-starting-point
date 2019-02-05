53b3193668d009eb1d7918e5558ebd64f5a1004c
aminabenabdallah <36482663+aminabenabdallah@users.noreply.github.com>
2019-02-04 21:16:03 -0600

    Initial commit

---
+++ app/src/main/java/com/example/android/quakereport/Earthquake.java
@@ -0,0 +1,28 @@
+package com.example.android.quakereport;
+
+public class Earthquake {
+    private String mMagnitude;
+    private String mLocation;
+    private long mTimeInMilliseconds;
+
+
+    public Earthquake(String magnitude, String location, long timeInMilliseconds) {
+        mMagnitude = magnitude;
+        mLocation = location;
+        mTimeInMilliseconds = timeInMilliseconds;
+    }
+
+    public String getMagnitude() {
+        return mMagnitude;
+    }
+
+    public String getLocation() {
+        return mLocation;
+    }
+
+    public long getTimeInMilliseconds() {
+        return mTimeInMilliseconds;
+    }
+
+
+}
